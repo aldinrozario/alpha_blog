9d6d45ff8bce4a82fef85be46f965631377eb21d
ROZARIO76 <aldinrozario76@gmail.com>
2022-10-31 14:28:47 +0530

    Create edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,11 +7,35 @@ class ArticlesController < ApplicationController
 	def create 
 		# render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		@article.save
+		if @article.save
+			flash[:notice] = "Article was successfully created"
+			redirect_to article_path(@article)
+		else 
+			render 'new'
+		end
 	end
 
 	def article_params
 		params.require(:article).permit(:title, :description)
 	end
+
+	def show 
+		@article = Article.find(params[:id])
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article was successfully updated"
+			redirect_to article_path(@article)
+		else 
+			render 'edit'
+		end
+	end
+
 
 end
